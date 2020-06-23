8b3830bf03b75a9811f0973959be56e6e90a8393
“Marc <marc_piantone@live.fr>
2020-06-23 13:58:13 -0400

    Answer exercise-3

--- workshop/exercise-3.js
+++ workshop/exercise-3.js
@@ -2,14 +2,23 @@
 // ----------
 
 let mostPopularFood = [
-  'bean burritos',
-  'poke bowl',
-  'chicken sliders',
-  'pork baby back ribs',
-  'chicken burritos',
-  'chicken sandwich',
-  'cauliflower rice bowl',
-  'chicken and waffle sliders',
-  'parmesan chicken',
-  'buffalo-flavored cauliflower',
+  "bean burritos",
+  "poke bowl",
+  "chicken sliders",
+  "pork baby back ribs",
+  "chicken burritos",
+  "chicken sandwich",
+  "cauliflower rice bowl",
+  "chicken and waffle sliders",
+  "parmesan chicken",
+  "buffalo-flavored cauliflower",
 ];
+
+const printInAlphabeticalOrder = (arr) => {
+  const alphaArr = [...arr];
+  alphaArr.sort().forEach((element) => {
+    console.log(`${element} (${arr.indexOf(element) + 1})`);
+  });
+};
+
+printInAlphabeticalOrder(mostPopularFood);
