e79fb4072064efa2ed2cdb65e837e68de0258820
“Marc <marc_piantone@live.fr>
2020-06-23 16:48:28 -0400

    Answer 5-6-7

--- workshop/exercise-7.js
+++ workshop/exercise-7.js
@@ -2,15 +2,28 @@
 // ----------
 
 let foodPairings = {
-  burgers: 'beer',
-  fish: 'white wine',
-  beef: 'red wine',
-  strawberries: 'cream',
+  burgers: "beer",
+  fish: "white wine",
+  beef: "red wine",
+  strawberries: "cream",
 };
 
 // createRecommendations function
 
-// printRecommendations function
+const createRecommendations = (arr) => {
+  let sides = Object.values(arr);
+  return Object.keys(arr).map((food, foodIndex) => {
+    return `With ${food}, it is best to have ${sides[foodIndex]}`;
+  });
+};
 
+// printRecommendations function
+const printRecommendations = (arr) => {
+  arr.forEach((element) => {
+    console.log(`- ${element}`);
+  });
+};
 // function call (done)
+//printRecommendations(createRecommendations(foodPairings));
+
 printRecommendations(createRecommendations(foodPairings));
