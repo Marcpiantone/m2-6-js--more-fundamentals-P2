8820299f5af0db6c503031dfa677579bbdea95a8
“Marc <marc_piantone@live.fr>
2020-06-23 13:05:38 -0400

    Answer exercise-1

--- workshop/exercise-1.js
+++ workshop/exercise-1.js
@@ -2,13 +2,23 @@
 // ----------
 
 let mostPopularFood = [
-  'poke bowl',
-  'chicken sliders',
-  'pork baby back ribs',
-  'chicken burritos',
-  'chicken sandwich',
-  'cauliflower rice bowl',
-  'chicken and waffle sliders',
-  'parmesan chicken',
+  "poke bowl",
+  "chicken sliders",
+  "pork baby back ribs",
+  "chicken burritos",
+  "chicken sandwich",
+  "cauliflower rice bowl",
+  "chicken and waffle sliders",
+  "parmesan chicken",
   undefined,
 ];
+
+const fixList = (arr, first, last) => {
+  arr.unshift(first);
+  arr[9] = last;
+  return arr;
+};
+
+console.log(
+  fixList(mostPopularFood, "bean burritos", "buffalo-flavored cauliflower")
+);
