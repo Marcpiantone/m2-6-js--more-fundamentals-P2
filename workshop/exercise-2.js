51df1da5446cd10b737229e35e6c3fd4beee7b04
“Marc <marc_piantone@live.fr>
2020-06-23 13:25:36 -0400

    Answer exercise-2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -1,5 +1,14 @@
 // Exercise 2
 // ----------
 
-const lateComers = ['Felicia', 'Charles', 'Terry'];
-const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
+const lateComers = ["Felicia", "Charles", "Terry"];
+const earlyBirds = ["Ellen", "Jerry", "Jasmine"];
+
+const printGuestOrder = (early, late) => {
+  const newArray = early.concat(late);
+  newArray.forEach((element) => {
+    console.log(`${newArray.indexOf(element) + 1}. ${element}`);
+  });
+};
+
+printGuestOrder(earlyBirds, lateComers);
